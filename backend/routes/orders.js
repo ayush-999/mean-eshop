777cd1d405f11865ad78ef78c16466614633c3d5
ayush-999 <ayushchaturvedi789@gmail.com>
2023-05-31 23:55:57 +0530

    order api added

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,14 +1,247 @@
-const {Order} = require('../models/order');
-const express = require('express');
+const { Order } = require("../models/order");
+const express = require("express");
+const { OrderItem } = require("../models/order-item");
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
-    const orderList = await Order.find();
+/**
+ * GET = http://localhost:3000/api/v1/orders
+ */
+router.get("/", (req, res) => {
+   Order.find()
+      .populate("user", "name")
+      .sort({ dateOrdered: -1 })
+      .then((orderList) => {
+         if (orderList) {
+            return res.status(200).send(orderList);
+         } else {
+            return res.status(500).json({
+               success: false,
+            });
+         }
+      })
+      .catch((err) => {
+         return res.status(400).json({
+            success: false,
+            error: err,
+         });
+      });
+});
 
-    if(!orderList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(orderList);
-})
+/**
+ * GET = http://localhost:3000/api/v1/categories/:id
+ */
+router.get("/:id", (req, res) => {
+   Order.findById(req.params.id)
+      .populate("user", "name")
+      .populate({
+         path: "orderItems",
+         populate: { path: "product", populate: "category" },
+      })
+      .then((order) => {
+         if (order) {
+            return res.status(200).send(order);
+         } else {
+            return res.status(404).json({
+               success: false,
+               message: "Order not found",
+            });
+         }
+      })
+      .catch((err) => {
+         return res.status(400).json({
+            success: false,
+            error: err,
+         });
+      });
+});
 
-module.exports =router;
+/**
+ * POST = http://localhost:3000/api/v1/orders
+ */
+router.post("/", (req, res) => {
+   Promise.all(
+      req.body.orderItems.map((orderItem) => {
+         let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product,
+         });
+         return newOrderItem
+            .save()
+            .then((savedOrderItem) => savedOrderItem._id);
+      })
+   )
+      .then((orderItemsIdsResolved) => {
+         Promise.all(
+            orderItemsIdsResolved.map((orderItemId) => {
+               return OrderItem.findById(orderItemId)
+                  .populate("product", "price")
+                  .then((orderItem) => {
+                     const totalPrice =
+                        orderItem.product.price * orderItem.quantity;
+                     return totalPrice;
+                  });
+            })
+         )
+            .then((totalPrices) => {
+               const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+               let order = new Order({
+                  orderItems: orderItemsIdsResolved,
+                  shippingAddress1: req.body.shippingAddress1,
+                  shippingAddress2: req.body.shippingAddress2,
+                  city: req.body.city,
+                  zip: req.body.zip,
+                  country: req.body.country,
+                  phone: req.body.phone,
+                  status: req.body.status,
+                  totalPrice: totalPrice,
+                  user: req.body.user,
+               });
+               order
+                  .save()
+                  .then((savedOrder) => {
+                     if (!savedOrder)
+                        return res
+                           .status(400)
+                           .send("the order cannot be created!");
+                     res.send(savedOrder);
+                  })
+                  .catch((error) => {
+                     res.status(500).send(
+                        "An error occurred while saving the order."
+                     );
+                  });
+            })
+            .catch((error) => {
+               res.status(500).send(
+                  "An error occurred while calculating total prices."
+               );
+            });
+      })
+      .catch((error) => {
+         res.status(500).send("An error occurred while saving order items.");
+      });
+});
+
+/**
+ * PUT = http://localhost:3000/api/v1/orders
+ */
+router.put("/:id", (req, res) => {
+   Order.findByIdAndUpdate(
+      req.params.id,
+      { status: req.body.status },
+      { new: true }
+   )
+      .then((order) => {
+         if (!order) {
+            res.status(400).send("The order cannot be updated!");
+         } else {
+            res.send(order);
+         }
+      })
+      .catch((err) => {
+         res.status(500).json({ success: false, error: err });
+      });
+});
+
+/**
+ * DELETE = http://localhost:3000/api/v1/orders/:id
+ */
+router.delete("/:id", (req, res) => {
+   Order.findByIdAndRemove(req.params.id)
+      .then((order) => {
+         if (order) {
+            const orderItemPromises = order.orderItems.map((orderItem) => {
+               return OrderItem.findByIdAndRemove(orderItem);
+            });
+
+            Promise.all(orderItemPromises)
+               .then(() => {
+                  res.status(200).json({
+                     success: true,
+                     message: "The order is deleted!",
+                  });
+               })
+               .catch((err) => {
+                  res.status(500).json({ success: false, error: err });
+               });
+         } else {
+            res.status(404).json({
+               success: false,
+               message: "Order not found!",
+            });
+         }
+      })
+      .catch((err) => {
+         res.status(500).json({ success: false, error: err });
+      });
+});
+
+/**
+ * GET = http://localhost:3000/api/v1/orders/get/totalsales
+ */
+router.get("/get/totalsales", (req, res) => {
+   Order.aggregate([
+      { $group: { _id: null, totalsales: { $sum: "$totalPrice" } } },
+   ])
+      .then((totalSales) => {
+         if (!totalSales) {
+            return res.status(400).send("The order sales cannot be generated");
+         }
+
+         res.send({ totalsales: totalSales.pop().totalsales });
+      })
+      .catch((error) => {
+         // Handle error here
+         res.status(500).send("An error occurred while fetching total sales");
+      });
+});
+
+/**
+ * GET = http://localhost:3000/api/v1/orders/get/count
+ */
+router.get("/get/count", async (req, res) => {
+   try {
+      const orderCount = await Order.countDocuments();
+
+      if (orderCount === 0) {
+         return res.status(500).json({ success: false });
+      }
+
+      res.status(200).json({
+         orderCount: orderCount,
+         success: true,
+      });
+   } catch (error) {
+      res.status(500).json({ success: false, error: error.message });
+   }
+});
+
+/**
+ * GET = http://localhost:3000/api/v1/orders/get/userorders/:userid
+ */
+router.get("/get/userorders/:userid", (req, res) => {
+   Order.find({ user: req.params.userid })
+      .populate({
+         path: "orderItems",
+         populate: {
+            path: "product",
+            populate: "category",
+         },
+      })
+      .sort({ dateOrdered: -1 })
+      .then((userOrderList) => {
+         if (!userOrderList) {
+            res.status(500).json({ success: false });
+         }
+         res.send(userOrderList);
+      })
+      .catch((error) => {
+         // Handle error here
+         res.status(500).json({
+            success: false,
+            error: "An error occurred while fetching user orders",
+         });
+      });
+});
+
+module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,13 +7,6 @@ const jwt = require("jsonwebtoken");
 /**
  * GET = http://localhost:3000/api/v1/users
  */
-// router.get(`/`, async (req, res) => {
-//   const userList = await User.find().select('-passwordHash');
-//   if (!userList) {
-//     res.status(500).json({ success: false });
-//   }
-//   res.status(200).send(userList);
-// });
 router.get("/", (req, res) => {
    User.find()
       // .select('-passwordHash')
@@ -37,15 +30,6 @@ router.get("/", (req, res) => {
 /**
  * GET = http://localhost:3000/api/v1/users/:id
  */
-// router.get('/:id', async (req, res) => {
-//   const user = await User.findById(req.params.id).select('-passwordHash');
-//   if (!user) {
-//     res
-//       .status(500)
-//       .json({ message: 'The user with the given ID was not found.' });
-//   }
-//   res.status(200).send(user);
-// });
 router.get("/:id", (req, res) => {
    User.findById(req.params.id)
       .select("-passwordHash")
@@ -70,25 +54,6 @@ router.get("/:id", (req, res) => {
 /**
  * POST = http://localhost:3000/api/v1/users
  */
-// router.post('/', async (req, res) => {
-//   let user = new User({
-//     name: req.body.name,
-//     email: req.body.email,
-//     passwordHash: bcrypt.hashSync(req.body.password, 10),
-//     phone: req.body.phone,
-//     isAdmin: req.body.isAdmin,
-//     street: req.body.street,
-//     apartment: req.body.apartment,
-//     zip: req.body.zip,
-//     city: req.body.city,
-//     country: req.body.country,
-//   });
-//   user = await user.save();
-//   if (!user) {
-//     return res.status(404).send('The user cannot be created!');
-//   }
-//   res.status(201).send(user);
-// });
 router.post("/", (req, res) => {
    let user = new User({
       name: req.body.name,
@@ -121,36 +86,6 @@ router.post("/", (req, res) => {
 /**
  * PUT = http://localhost:3000/api/v1/users/:id
  */
-// router.put('/:id', async (req, res) => {
-//   const userExist = await User.findById(req.params.id);
-//   let newPassword;
-//   if (req.body.password) {
-//     newPassword = bcrypt.hashSync(req.body.password, 10);
-//   } else {
-//     newPassword = userExist.passwordHash;
-//   }
-
-//   const user = await User.findByIdAndUpdate(
-//     req.params.id,
-//     {
-//       name: req.body.name,
-//       email: req.body.email,
-//       passwordHash: newPassword,
-//       phone: req.body.phone,
-//       isAdmin: req.body.isAdmin,
-//       street: req.body.street,
-//       apartment: req.body.apartment,
-//       zip: req.body.zip,
-//       city: req.body.city,
-//       country: req.body.country,
-//     },
-//     { new: true }
-//   );
-//   if (!user) {
-//     return res.status(400).send('The user cannot be updated!');
-//   }
-//   res.status(201).send(user);
-// });
 router.put("/:id", (req, res) => {
    User.findById(req.params.id)
       .then(async (userExist) => {
@@ -189,37 +124,6 @@ router.put("/:id", (req, res) => {
 });
 
 /**
- * PUT = http://localhost:3000/api/v1/users/login
- */
-router.post("/login", async (req, res) => {
-   const user = await User.findOne({
-      email: req.body.email,
-   });
-   const secret = process.env.secret;
-   if (!user) {
-      return res.status(400).send("The user not found");
-   }
-   if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
-      const token = jwt.sign(
-         {
-            userId: user.id,
-            isAdmin: user.isAdmin,
-         },
-         secret,
-         {
-            expiresIn: "1d",
-         }
-      );
-      res.status(200).send({
-         user: user.email,
-         token: token,
-      });
-   } else {
-      return res.status(400).send("Password is wrong!");
-   }
-});
-
-/**
  * DELETE = http://localhost:3000/api/v1/users/:id
  */
 router.delete("/:id", (req, res) => {
@@ -242,6 +146,41 @@ router.delete("/:id", (req, res) => {
             success: false,
             error: err,
          });
+      });
+});
+
+/**
+ * PUT = http://localhost:3000/api/v1/users/login
+ */
+router.post("/login", (req, res) => {
+   User.findOne({ email: req.body.email })
+      .then((user) => {
+         const secret = process.env.secret;
+         if (!user) {
+            return res.status(400).send("The user not found");
+         }
+         if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+            const token = jwt.sign(
+               {
+                  userId: user.id,
+                  isAdmin: user.isAdmin,
+               },
+               secret,
+               {
+                  expiresIn: "1d",
+               }
+            );
+            res.status(200).send({
+               user: user.email,
+               token: token,
+            });
+         } else {
+            return res.status(400).send("Password is wrong!");
+         }
+      })
+      .catch((error) => {
+         console.error(error);
+         res.status(500).send("An error occurred");
       });
 });
 
