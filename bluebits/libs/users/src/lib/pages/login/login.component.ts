62fd79504429623ce8ebb44f93157ae84dd6e132
ayush-999 <ayushchaturvedi789@gmail.com>
2023-07-01 20:09:41 +0530

    all admin bugs fixed

--- bluebits/libs/users/src/lib/pages/login/login.component.ts
+++ bluebits/libs/users/src/lib/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { MessageService } from 'primeng/api';
 import { LocalstorageService } from '../../services/localstorage.service';
 
 @Component({
@@ -16,7 +17,13 @@ export class LoginComponent implements OnInit {
     authError = false;
     authMessage = 'Email or Password are wrong';
 
-    constructor(private formBuilder: FormBuilder, private auth: AuthService, private localstorageService: LocalstorageService, private router: Router) {}
+    constructor(
+        private formBuilder: FormBuilder,
+        private auth: AuthService,
+        private localstorageService: LocalstorageService,
+        private router: Router,
+        private messageService: MessageService
+    ) {}
 
     ngOnInit(): void {
         this._initLoginForm();
@@ -33,23 +40,30 @@ export class LoginComponent implements OnInit {
     isLoading = false;
     onSubmit() {
         this.isSubmitted = true;
+        if (this.loginFormGroup.invalid) {
+            return;
+        }
 
-        if (this.loginFormGroup.invalid) return;
+        this.isLoading = true;
 
-        this.auth.login(this.loginForm.email.value, this.loginForm.password.value).subscribe(
-            (user) => {
-                this.authError = false;
-                this.localstorageService.setToken(user.token);
-                // this.router.navigate(['/']);
-                this.router.navigate(['/dashboard']);
-            },
-            (error: HttpErrorResponse) => {
-                this.authError = true;
-                if (error.status !== 400) {
-                    this.authMessage = 'Error in the Server, please try again later!';
+        setTimeout(() => {
+            this.isLoading = false;
+
+            this.auth.login(this.loginForm.email.value, this.loginForm.password.value).subscribe(
+                (user) => {
+                    this.authError = false;
+                    this.localstorageService.setToken(user.token);
+                    // this.router.navigate(['/']);
+                    this.router.navigate(['/dashboard']);
+                },
+                (error: HttpErrorResponse) => {
+                    this.authError = true;
+                    if (error.status !== 400) {
+                        this.authMessage = 'Error in the Server, please try again later!';
+                    }
                 }
-            }
-        );
+            );
+        }, 2000);
     }
 
     get loginForm() {
